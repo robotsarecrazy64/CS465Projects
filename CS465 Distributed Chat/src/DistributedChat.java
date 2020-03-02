55e8421d8f959d335c181de8374551fc91427245
jpj46 <48301533+jpj46@users.noreply.github.com>
2020-03-01 19:27:24 -0700

    Let's try this instead

--- CS465 Distributed Chat/src/DistributedChat.java
+++ CS465 Distributed Chat/src/DistributedChat.java
@@ -1,68 +1,164 @@
-import java.net.*; 
-import java.io.*; 
-import java.util.*; 
-public class DistributedChat 
-{ 
-	private static final String TERMINATE = "Exit"; 
-	static String name; 
-	static volatile boolean finished = false; 
-	public static void main(String[] args) 
-	{ 
-		if (args.length != 2) 
-			System.out.println("Two arguments required: <multicast-host> <port-number>"); 
-		else
-		{ 
-			try
-			{ 
-				InetAddress group = InetAddress.getByName(args[0]); 
-				int port = Integer.parseInt(args[1]); 
-				Scanner sc = new Scanner(System.in); 
-				System.out.print("Enter your name: "); 
-				name = sc.nextLine(); 
-				MulticastSocket socket = new MulticastSocket(port);
-            socket.setBroadcast(true);
-			
-				// Since we are deploying 
-				socket.setTimeToLive(0); 
-				//this on localhost only (For a subnet set it as 1) 
-				
-				socket.joinGroup(group); 
-				Thread t = new Thread(new
-				DCClientHandler(socket,group,port)); 
-			
-				// Spawn a thread for reading messages 
-				t.start(); 
-				
-				// sent to the current group 
-				System.out.println("Start typing messages...\n"); 
-				while(true) 
-				{ 
-					String message; 
-					message = sc.nextLine(); 
-					if(message.equalsIgnoreCase(DistributedChat.TERMINATE)) 
-					{ 
-						finished = true; 
-						socket.leaveGroup(group); 
-						socket.close(); 
-						break; 
-					} 
-					message = name + ": " + message; 
-					byte[] buffer = message.getBytes(); 
-					DatagramPacket datagram = new
-					DatagramPacket(buffer,buffer.length,group,port); 
-					socket.send(datagram); 
-				} 
-			} 
-			catch(SocketException se) 
-			{ 
-				System.out.println("Error creating socket"); 
-				se.printStackTrace(); 
-			} 
-			catch(IOException ie) 
-			{ 
-				System.out.println("Error reading/writing from/to socket"); 
-				ie.printStackTrace(); 
-			} 
-		} 
-	} 
-} 
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.TextArea;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class DistributedChat extends Frame implements Runnable {
+    // text output of all connections
+    private final TextArea textArea;
+    // broadcast and receive of UDP; used for TCP connection(s) to peer(s)
+    private final Broadcasts broadcasts;
+    // list of all sockets for TCP output
+    private final ArrayList<Socket> sockets;
+    // storage for text data
+    private StringBuilder lines;
+    // continue running application?
+    private boolean run = true;
+
+    public DistributedChat() {
+        // create field objects
+        sockets = new ArrayList<>();
+        lines = new StringBuilder();
+        textArea = new TextArea(20, 80);
+        // set focusable to false to ensure keys are captured by frame
+        textArea.setFocusable(false);
+        // monospace ftw
+        textArea.setFont(Font.decode("monospaced"));
+        // the only gui object is the text area
+        add(textArea);
+        pack();
+
+        // start socket server to accept incoming connections
+        new Thread(this).start();
+
+        // instantiate and assign window listener and key listener to frame
+        FrameListener frameListener = new FrameListener(this);
+        addWindowListener(frameListener);
+        addKeyListener(frameListener);
+
+        // late initialize of UDP broadcast and receive, to ensure needed
+        // objects are instantiated
+        broadcasts = new Broadcasts(this);
+
+        setVisible(true);
+    }
+
+    // global quit method shuts down everything and exits
+    public void quit() {
+        run = false;
+        broadcasts.quit();
+        System.exit(0);
+    }
+
+    // method called by key listener
+    public void keyTyped(KeyEvent ke) {
+        int i;
+        synchronized (sockets) {
+            // iterate through all sockets, and flush character through
+            for (i = 0; i < sockets.size(); i++) {
+                try {
+                    Socket s = sockets.get(i);
+                    PrintWriter pw = new PrintWriter(s.getOutputStream());
+                    pw.print(String.valueOf(ke.getKeyChar()));
+                    pw.flush();
+                } catch (IOException ex) {
+                    // remove socket, continue to any next if exception occurs
+                    // (socket closed)
+                    ex.printStackTrace();
+                    sockets.remove(i);
+                    continue;
+                }
+            }
+        }
+    }
+
+    // method called by per-connection thread defined in socketStream
+    public void putChar(int ch) {
+        // check for backspace and space for delete,
+        // otherwise put character into buffer,
+        // and show updated buffer
+        if (ch == 8 && lines.length() > 0)
+            lines.delete(lines.length() - 1, lines.length());
+        else
+            lines.append((char)ch);
+        synchronized (textArea) {
+            textArea.setText(lines.toString() + '.');
+        }
+    }
+
+    // method called by UDP listener
+    // exits if connection fails
+    void newAddress(InetAddress address) {
+        synchronized (sockets) {
+            // check if already connected to address, and exit if true
+            for (Socket addr: sockets)
+                if (addr.getInetAddress().getHostAddress()
+                        .equals(address.getHostAddress()))
+                    return;
+            // create a new socket and add it to transmission pool
+            Socket s;
+            try {
+                s = new Socket(address.getHostAddress(), Globals.TCPPORT);
+            } catch (IOException ex) {
+                return;
+            }
+            sockets.add(s);
+        }
+    }
+
+    // called by socket server thread
+    // defines a thread for each connection,
+    // which calls putChar for every received character
+    // exits thread if error occurs (socket closed)
+    private void socketStream(final Socket s) {
+        final InputStream is;
+        try {
+            is = s.getInputStream();
+        } catch (IOException ex) {
+            return;
+        }
+        final InputStreamReader isr = new InputStreamReader(is);
+        final BufferedReader br = new BufferedReader(isr);
+        new Thread(new Runnable() {
+            public void run() {
+                while (run && s.isConnected()) {
+                    try {
+                        if (br.ready())
+                            putChar(br.read());
+                    } catch (IOException ex) {
+                        return;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // socket server accepts incoming connection,
+    // and creates a thread to pass characters to the screen
+    public void run() {
+        try {
+            ServerSocket ss = new ServerSocket(Globals.TCPPORT);
+            while (ss.isBound() && run) {
+                socketStream(ss.accept());
+            }
+            quit();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            quit();
+        }
+    }
+
+    // application entry
+    public static void main(String[] args) {
+        new DistributedChat();
+    }
+}
---
+++ CS465 Distributed Chat/src/FrameListener.java
@@ -0,0 +1,63 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class FrameListener implements WindowListener, KeyListener {
+    private final DistributedChat parent;
+
+    public FrameListener(DistributedChat parentArg) {
+        parent = parentArg;
+    }
+
+    @Override
+    public void windowOpened(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent we) {
+        parent.quit();
+    }
+
+    @Override
+    public void windowClosed(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent we) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent we) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        parent.keyTyped(ke);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+
+    }
+
+}
