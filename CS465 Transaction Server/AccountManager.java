8459526ef872a9da9d356af2afdffb31a6a1003f
John <jpj46@nau.edu>
2020-03-13 11:38:49 -0700

    Updated comments and skeleton code

--- CS465 Transaction Server/AccountManager.java
+++ CS465 Transaction Server/AccountManager.java
@@ -3,35 +3,40 @@ import transaction.server.transaction.Transaction;
 import transaction.server.TransactionServer;
 import transaction.server.lock.LockTypes;
 
+/*
+ * Class that manages the accounts and their transactions
+ */
 public class AccountManager implements LockTypes
 {
-    
-    static ServerSocket server;
     private static ArrayList<Account> accounts; // transaction ids of current holders
     private static int numberAccounts;
     private static int initialBalance;
     
-    
+    // Constructor for the class which adds all accounts to an array
     public AccountManager(int numberAccounts, int initialBalance) 
     {
         
     }
     
+    // Retrieves a specific account from the list of accounts
     public Account getAccount(int accountNumber)
     {
        
     }
     
-    public void ArrayList<Account> getAccounts() 
+    // Returns an array of all the accounts
+    public ArrayList<Account> getAccounts() 
     {
         
     }
     
+    // Get the balance of the account
     public int read(int accountNumber, Transaction transaction)
     {
        
     }
     
+    // Set the balance of the account
     public int write(int accountNumber, Transaction transaction, int balance)
     {
        
