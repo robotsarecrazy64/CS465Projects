378574e80fdc98214d7813f99a8e9bcb388483a0
John <jpj46@nau.edu>
2020-03-13 11:47:49 -0700

    updated comments

--- CS465 Transaction Server/AccountManager.java
+++ CS465 Transaction Server/AccountManager.java
@@ -15,30 +15,37 @@ public class AccountManager implements LockTypes
     // Constructor for the class which adds all accounts to an array
     public AccountManager(int numberAccounts, int initialBalance) 
     {
-        
+        for(int i = 0; i < numberAccounts; i++)
+        {
+           //TODO: add accounts to accounts list 
+        }
     }
     
     // Retrieves a specific account from the list of accounts
     public Account getAccount(int accountNumber)
     {
-       
+       return accounts.get(accountNumber);
     }
     
     // Returns an array of all the accounts
     public ArrayList<Account> getAccounts() 
     {
-        
+        return accounts;
     }
     
     // Get the balance of the account
     public int read(int accountNumber, Transaction transaction)
     {
-       
+       // get account and read balance
+       account = getAccount(accountNumber);
+       return account.getBalance();
     }
     
     // Set the balance of the account
     public int write(int accountNumber, Transaction transaction, int balance)
     {
-       
+       // get account and set balance
+       account.setBalance(balance);
+       return balance;
     }
 }
--- CS465 Transaction Server/LockManager.java
+++ CS465 Transaction Server/LockManager.java
@@ -6,13 +6,13 @@ public class LockManager
         Finds the lock associated with the object if it exists and adds it to the hash table
     */
     public void setLock(Object object, TrasnactionManager transaction, LockType type )
-	{
+	 {
         Lock newLock = Lock();
-		newLock.acquire();
+		  newLock.acquire();
     }
 
     public synchronized void unlock(TransID trans)
-	{
+	 {
         
     }
 }
