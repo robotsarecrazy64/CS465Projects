c0b2914919839c7c004cc1c771a76cc3eb030e92
Jessica <jls924@nau.edu>
2020-03-13 10:24:16 -0700

    added account vector

--- CS465 Transaction Server/AccountManager.java
+++ CS465 Transaction Server/AccountManager.java
@@ -1,7 +1,8 @@
 public class AccountManager {
     
     static ServerSocket server;
-    
+    private Vector<E> accounts; // transaction ids of current holders
+	
     public AccountManager() {
         //  
     }
