7cffb867988984697e00e712d0d03b2a5b9a52cc
Jessica <jls924@nau.edu>
2020-03-28 17:31:21 -0700

    fixing up acquire conditionals and added new lock type

--- CS465 Transaction Server/src/Lock.java
+++ CS465 Transaction Server/src/Lock.java
@@ -10,41 +10,71 @@ public class Lock implements LockTypes
 	/**
         Class Variables
     */
-    private Object lockObject; // the object that lock is protecting
-    private Vector<Transaction> holders; // transaction ids of current holders
-    private LockType lockType; // current type of lock
+	
+	// TODO: add comments describing each variable
+	private final Account account;
+	
+	private int currentLockType;
+	
+	private final ArrayList<Transaction> lockHolders;
+	
+	private final HashMap<Transaction, Object[]> lockRequestors;
+	
+	public Lock(Account account)
+	{
+		this.lockHolders = new ArrayList();
+		this.lockRequestors = new HashMap();
+		this.account = account;
+		this.currentLockType = EMPTY_LOCK;
+	}
 
 	/**
         Lock the current transaction
     */
-    public synchronized void acquire(Transaction transaction, LockType curLockType) 
+    public synchronized void acquire(Transaction transaction, int newLockType) 
 	{
 		// while another transaction holds the lock needed
-        while (checkConflict(curLockType))
+        while (checkConflict(transaction, newLockType))
 		{
 			try 
 			{
+				addLockRequestor(transaction, newLockType);
 				wait();
+				removeLockRequestor(transaction);
 			} catch (InterruptedException except)
 			{
 				// send error
 			}
 		}
-		if (holders.isEmpty()) // no PIDs hold lock
+		
+		if (lockHolders.isEmpty()) // no PIDs hold lock
 		{
-			holders.addElement(transaction);
-			lockType = curLockType;
+			lockHolders.add(transaction);
+			currentLockType = newLockType;
+			transaction.addLock(this);
 		}
-		else if ()// another transaction holds the lock, share it
+		
+		else if (!lockHolders.contains(transaction))
 		{
-			if() // this transaction not a holder
+			Iterator <Transaction> lockIterator = lockHolders.iterator();
+			Transaction otherTransaction;
+			while (lockIterator.hasNext())
 			{
-				holders.addElement(transaction);
+				otherTransaction = lockIteractor.next();
 			}
+			
+			lockHolders.add(transaction);
+			transaction.addLock(this);
 		}
-		else if() // this transation is a holder but needs a more exclusive lock
+
+		else if(lockHolders.size() == 1 && currentLockType == READ_LOCK && newLockType == WRITE_LOCK) // this transation is a holder but needs a more exclusive lock
 		{
-			lockType.promote();
+			currentLockType = newLockType;
+		}
+		
+		else
+		{
+			// do nothing
 		}
     }
 
@@ -61,12 +91,29 @@ public class Lock implements LockTypes
 	/**
         Checks if the lock needed is already in use
     */
-	public synchronized bool checkConflict(LockType curLockType) 
+	public synchronized bool checkConflict(Transaction transaction, LockType curLockType) 
 	{
-		// implement method that checks for conflict
-		for( int iter = 0; iter < holders.size(); iter++)
+		if (lockHolders.isEmpty())
 		{
-			if(iter.lockType) // if the lock is in use in one of the transactions
+			return false;
+		}
+		else if (lockholders.size() == 1 && lockHolders.contains(transaction))
+		{
+			return false;
+		}
+		else 
+		{ // new ting
+			Iterator <Transaction> lockIterator = lockHolders.iterator();
+			Transaction otherTransaction;
+			while (lockIterator.hasNext())
+			{
+				otherTransaction = lockIteractor.next();
+			}
+		}
+		// implement method that checks for conflict
+		for( int iter = 0; iter < lockHolders.size(); iter++)
+		{
+			if(lockeHolder.at(iter).lockType) // if the lock is in use in one of the transactions
 			{
 				// there is a conflict
 				return true;
